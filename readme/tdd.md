71ccb8e946349c020282aff6d0140d9513acd034
ehddnr301 <dy950328@gmail.com>
2020-07-07 15:29:54 +0900

    beforeTest

---
+++ readme/tdd.md
@@ -0,0 +1,13 @@
+# TDD
+
+## 목적
+
+- Test Schemas
+- Test Resolvers
+
+## 테스트
+
+- Static Type checking
+- Unit tests
+- Integration tests
+- Ene to End tests
--- src/api/User/createUser/createUser.resolvers.ts
+++ src/api/User/createUser/createUser.resolvers.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import User from "../../../models/User";
 import { CreateUserMutationArgs } from "../../../types/graph";
 
@@ -5,9 +6,10 @@ export default {
   Mutation: {
     createUser: async (_: any, args: CreateUserMutationArgs) => {
       const { username, password, email } = args;
+      const hashedPassword = bcrypt.hash(password, 10);
       const user = await User.create({
         username,
-        password,
+        password: hashedPassword,
         email,
       });
       return user;
